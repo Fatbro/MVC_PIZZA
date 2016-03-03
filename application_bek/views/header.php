cc5f0710123c61e9ca7d7859bbc6322ba987509e
Fatbro <bkossunov@gmail.com>
2016-03-03 10:05:13 +0600

    123

---
+++ application_bek/views/header.php
@@ -0,0 +1,12 @@
+<html> 
+<head>
+	<title>CodeIgniter Tutorial</title>
+</head>
+
+<body>
+	<h1><?php echo $title; ?></h1>
+		<em>&copy; 2016</em>
+
+
+</body> 
+</html>
---
+++ application_bek/views/news/success.php
@@ -0,0 +1,1 @@
+<h1> Github setting up</h1>
